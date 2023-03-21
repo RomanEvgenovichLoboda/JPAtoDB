e2e67d0f04d5130151aadbd292841ad22275261b
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-03-21 17:46:28 +0200

    Initial commit

---
+++ src/main/java/JPASpring/JPAtoDB/JpAtoDbApplication.java
@@ -0,0 +1,13 @@
+package JPASpring.JPAtoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAtoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAtoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JPASpring/JPAtoDB/JpAtoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package JPASpring.JPAtoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAtoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
